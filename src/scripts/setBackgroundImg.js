728c070884b3e81963e8cd1b6f9c639e66f1a40f
June <danpa725@cau.ac.kr>
2021-01-23 23:33:19 +0900

    Make background-img change with current users time.

--- src/scripts/setBackgroundImg.js
+++ src/scripts/setBackgroundImg.js
@@ -3,19 +3,19 @@ const body = document.querySelector("body");
 const IMG_URL = [
     "../images/dayStart.jpg",
     "../images/afetrnoon.jpg",
-    "../images/miidNight.jpg",
+    "../images/midNight.jpg",
     "../images/deepNight.jpg",
 ];
 
 const setBackground = (hours) => {
     if (6 < hours && hours < 12) {
-        body.style.backgroundImage = `url(${IMG_URL[0]})`;
+        body.classList.add("day-start");
     } else if (12 < hours && hours < 18) {
-        body.style.backgroundImage = `url(${IMG_URL[1]})`;
+        body.classList.add("afternoon");
     } else if (18 < hours && hours < 24) {
-        body.style.backgroundImage = `url(${IMG_URL[2]})`;
+        body.classList.add("midnight");
     } else if (0 < hours && hours < 6) {
-        body.style.backgroundImage = `url(${IMG_URL[3]})`;
+        body.classList.add("deep-night");
     }
 };
 
